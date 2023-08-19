9aee950f8540790ac79119daad40c4ff1feb0cb9
sntx <sntx.htqjd@simplelogin.com>
2023-08-19 22:00:26 +0200

    moved the hm module to a submodule of nixosmodules

--- flake.nix
+++ flake.nix
@@ -83,10 +83,7 @@
           drv = anyrun;
         };
 
-        homeManagerModules = {
-          anyrun = import ./hm-module.nix self;
-          default = anyrun;
-        };
+        nixosModules.home-manager = import ./hm-module.nix self;
 
         devShells.default = pkgs.mkShell {
           inputsFrom = builtins.attrValues self.checks.${system};
